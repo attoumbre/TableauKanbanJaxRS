439762f18e81481057e9b5ed9a85bf024ebc859a
attoumbre <juvenalak@gmail.com>
2022-02-16 11:13:26 +0100

    delete pet

--- src/main/java/fr/istic/taa/jaxrs/TestApplication.java
+++ src/main/java/fr/istic/taa/jaxrs/TestApplication.java
@@ -21,7 +21,6 @@ import java.util.Set;
 
 import javax.ws.rs.core.Application;
 
-import fr.istic.taa.jaxrs.rest.PetResource;
 import fr.istic.taa.jaxrs.rest.SwaggerResource;
 import fr.istic.taa.jaxrs.rest.Usersresource;
 import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
@@ -34,7 +33,6 @@ public class TestApplication extends Application {
 
         final Set<Class<?>> clazzes = new HashSet<Class<?>>();
 
-        clazzes.add(PetResource.class);
         clazzes.add(Usersresource.class);
         clazzes.add(OpenApiResource.class);
         clazzes.add(SwaggerResource.class);
