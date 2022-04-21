61b656573b58692ac6f42452af4428bef58b02ca
Yeo Nadege <nadege.yeo@etudiant.univ-rennes1.fr>
2022-04-21 18:17:19 +0100

    requete pour recuperer tableau dun user

--- src/main/java/fr/istic/taa/jaxrs/dao/generic/TableauDao.java
+++ src/main/java/fr/istic/taa/jaxrs/dao/generic/TableauDao.java
@@ -1,5 +1,7 @@
 package fr.istic.taa.jaxrs.dao.generic;
 
+import java.util.List;
+
 import fr.istic.taa.jaxrs.domain.Tableau;
 
 public class TableauDao extends AbstractJpaDao<Long, Tableau>{
@@ -7,5 +9,11 @@ public class TableauDao extends AbstractJpaDao<Long, Tableau>{
 	public TableauDao() {
 		super(Tableau.class);
 	}
-
+	
+	
+    public List<Tableau> findTabByUser(Long id) {
+		String query = "select t from Tableau as t inner join Users as u on t.userT.id = u.id where u.id = :user ";
+		return  (List<Tableau>) EntityManagerHelper.getEntityManager().createQuery(query, Tableau.class)
+				.setParameter("user", id).getResultList();
+	}
 }
--- src/main/java/fr/istic/taa/jaxrs/rest/TableauResource.java
+++ src/main/java/fr/istic/taa/jaxrs/rest/TableauResource.java
@@ -10,11 +10,14 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import fr.istic.taa.jaxrs.dao.generic.TableauDao;
 import fr.istic.taa.jaxrs.domain.Tableau;
 import fr.istic.taa.jaxrs.domain.Tags;
+import fr.istic.taa.jaxrs.domain.Users;
+import fr.istic.taa.jaxrs.dto.UserDto;
 
 
 @Path("/tableau")
@@ -63,4 +66,15 @@ public class TableauResource {
 				    
 				    return Response.ok().entity("SUCCESS").build();
 				  }
+				
+				@POST
+				@Path("/perso/{i}")
+				@Produces(MediaType.APPLICATION_JSON)
+				@Consumes(MediaType.APPLICATION_JSON)
+				public List<Tableau> getTabByUser (@PathParam("i") Long i) {
+					
+					return this.dao.findTabByUser(i);
+					
+				}
+				
 }
